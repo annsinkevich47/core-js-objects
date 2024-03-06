db6d7ff54b039a3f68334d3b113c7a9758bcda19
annsinkevich47 <anneta.sinkevich@mail.ru>
2024-03-06 22:00:16 +0300

    fix: function name fix

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -209,7 +209,7 @@ function sellTickets(queue) {
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
+  this.getArea = function area() {
     return this.width * this.height;
   };
 }
